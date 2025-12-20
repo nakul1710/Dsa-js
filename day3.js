3848f4604c9ac089d69e609a893e5541f98b8c76
nakulyadav <nakulyadav1710@gmail.com>
2025-12-20 21:20:11 +0530

    day4-leetcode

--- day3.js
+++ day3.js
@@ -182,25 +182,20 @@ var search = function(nums, target) {
 
 //8. Find peak Element
 
-var findPeakElement = function(nums) {
-    let n = nums.length;
-
-    if (n === 1) return 0;
-    if (nums[0] > nums[1]) return 0;
-    if (nums[n - 1] > nums[n - 2]) return n - 1;
-
-    let start = 1;
-    let end = n - 2;
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
 
     while (start <= end) {
         let mid = Math.floor((start + end) / 2);
 
-        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+        if (nums[mid] === target) {
             return mid;
-        } else if (nums[mid] < nums[mid + 1]) {
+        } else if (target > nums[mid]) {
             start = mid + 1;
         } else {
             end = mid - 1;
         }
     }
+    return start;
 };
